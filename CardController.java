b69c2c2d4efd7cccc993141d8868fa6d631e7629
Ashwanth222 <ashwanthrao22@gmail.com>
2021-03-13 13:01:30 +0530

    First commit

---
+++ CardController.java
@@ -0,0 +1,57 @@
+/*public class CardController{
+	
+	//variables-instance
+	String cardId;
+	long cardNumber;
+	String expiryDate;
+	int CVV;
+	String cardType;
+}*/
+
+	//methods
+
+public class CardController{
+
+
+ 	public static void main(String args[]){
+
+	
+		System.out.println("main method starts");
+
+		Card c1 = new Card();
+
+		System.out.println(c1.cardNumber);
+		System.out.println(c1.expiryDate);
+		System.out.println(c1.CVV);
+		System.out.println(c1.cardType);
+		System.out.println(c1.cardId);
+		
+
+		System.out.println("============");
+
+		Card d1 = new Card();
+
+		System.out.println(d1.cardNumber);
+		System.out.println(d1.expiryDate);
+		System.out.println(d1.CVV);
+		System.out.println(d1.cardType);
+		System.out.println(d1.cardId);
+
+		System.out.println("main method ends");
+		
+
+
+	}
+}
+		
+
+		
+
+				
+
+		
+
+
+
+
+
