dc1760f77884561a92e8800ededdb8c79a206129
Ashwanth222 <ashwanthrao22@gmail.com>
2021-06-22 18:23:48 +0530

    sixtyhcommit

---
+++ EmployeServices/src/main/java/com/employ/service/EmployeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.employ.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeServicesApplication.class, args);
+	}
+
+}
---
+++ EmployeServices/src/test/java/com/employ/service/EmployeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employ.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
